72969a97bf7c959d6ee33d11efc398f1c82fa658
Tonyyyyyyyyyyyy <ea103java28.com>
2025-03-07 22:42:51 +0800

    Revise goroutine.go

--- goroutine.go
+++ goroutine.go
@@ -5,16 +5,23 @@ import (
 )
 
 func main(){
-	fmt.Println("開始找質數")
+	fmt.Println("開始找質數.....")
 	num := 90000
+	counter := 0
 	start := time.Now()
+	slice := []int{}
+
 	for i := 1; i<=num; i++ {
 		if isPrime(i){
-			fmt.Println(i)
+			// fmt.Println(i)
+			slice = append(slice, i)
+			counter++
 		}
 	}
 	end := time.Now()
-	fmt.Println(end.Unix() - start.Unix(), "seconds")
+	fmt.Println("no use goroutine need", end.Unix() - start.Unix(), "seconds")
+	fmt.Println("total primes is ", counter)
+
 }
 
 func isPrime(num int) bool { // 找質數
